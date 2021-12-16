7d52ce87756cec1563f3e5058c43e8871a067835
mamatha b <mamathabalireddy@gmail.com>
2021-12-16 22:59:02 +0530

    jst

--- sample.java
+++ sample.java
@@ -1,8 +1,5 @@
 executing git commands wthout using any IDE.
 updatng the flie.
 updatng file fter creating branch.
-<<<<<<< HEAD
 checkng the cmng cherry-pickiooooooooooooooo.
-=======
-stge cmnd
->>>>>>> db4c38f... updted stash
+

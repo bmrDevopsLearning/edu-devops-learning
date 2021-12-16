768e8a2a4ebf1395499f00c7269fa88cdf27a673
mamatha b <mamathabalireddy@gmail.com>
2021-12-16 22:43:12 +0530

    cherry-pck

--- sample.java
+++ sample.java
@@ -1,4 +1,8 @@
 executing git commands wthout using any IDE.
 updatng the flie.
 updatng file fter creating branch.
+<<<<<<< HEAD
 checkng the cmng cherry-pickiooooooooooooooo.
+=======
+stge cmnd
+>>>>>>> db4c38f... updted stash
